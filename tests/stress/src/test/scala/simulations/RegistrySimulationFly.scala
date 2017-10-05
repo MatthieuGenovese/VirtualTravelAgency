83caf87b2315847d7835b10ea308b0af38618a24
Michael Liechtensteger <lm203441@etu.unice.fr>
2017-10-05 23:19:34 +0200

    ajout d un test gatling sur les vols

---
+++ tests/stress/src/test/scala/simulations/RegistrySimulationFly.scala
@@ -0,0 +1,67 @@
+package simulations
+
+import java.util.UUID
+
+import scala.language.postfixOps
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+
+class RegistrySimulationFly extends Simulation {
+
+  val httpConf =
+    http
+      .baseURL("http://localhost:9080/flyreservation-service-document/")
+      .acceptHeader("application/json")
+      .header("Content-Type", "application/json")
+
+  val stressSample =
+    scenario("Registering Fly")
+        .repeat(10)
+        {
+          exec(session =>
+            session.set("destination", UUID.randomUUID().toString)
+          )
+            .exec(
+              http("registering a fly")
+                .post("registry")
+                .body(StringBody(session => buildRegister(session)))
+                .check(status.is(200))
+            )
+            .pause(1 seconds)
+            .exec(
+              http("retrieving a fly")
+                .post("registry")
+                .body(StringBody(session => buildRetrieve(session)))
+                .check(status.is(200))
+            )
+        }
+
+  def buildRegister(session: Session): String = {
+    val destination = session("destination").as[String]
+    raw"""{
+      "event": "REGISTER",
+      "flyreservation": {
+        "date": "2017-09-30",
+        "isDirect": "true",
+        "destination": "$destination",
+        "price": "543",
+        "stops": [{"stop":"abc"},{"stop":"def"}]
+      }
+    }""""
+  }
+
+
+  def buildRetrieve(session: Session): String = {
+    val destination = session("destination").as[String]
+    raw"""{
+      "event": "RETRIEVE",
+      "destination": "$destination"
+    }""""
+  }
+
+  setUp(stressSample.inject(rampUsers(20) over (10 seconds)).protocols(httpConf))
+}
