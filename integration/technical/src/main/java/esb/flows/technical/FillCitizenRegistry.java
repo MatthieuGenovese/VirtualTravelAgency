e6b98667a24f28b9bd7ae64cd5f75b240fda5e01
MatthieuGenovese <gm102445@etu.unice.fr>
2017-10-13 22:26:03 +0200

    modification d'un docker file et du came-context

--- integration/technical/src/main/java/esb/flows/technical/FillCitizenRegistry.java
+++ integration/technical/src/main/java/esb/flows/technical/FillCitizenRegistry.java
@@ -14,26 +14,29 @@ public class FillCitizenRegistry extends RouteBuilder {
 
     private static final String CSV_INPUT_DIRECTORY   = "file:/servicemix/camel/input";
     private static final String REGISTER_A_CITIZEN    = "activemq:registerCitizen";
-    private static final String REGISTRATION_ENDPOINT = "http://tcs-citizens:8080/tcs-service-document/registry";
-    private static final String GET_A_FLIGHT = "http://localhost:9080/flightreservation-service-document/registry";
+    private static final String REGISTRATION_ENDPOINT = "http://localhost:9080/flightreservation-service-document/registry";
+    public static final String GET_A_FLIGHT    = "activemq:getCitizenInfo";
+    private static final String GET_A_FLIGHT_ENDPOINT = "http://localhost:9080/flightreservation-service-document/registry";
 
 
     private static final ExecutorService WORKERS = Executors.newFixedThreadPool(5);
-
     @Override
     public void configure() throws Exception {
-        from(GET_A_FLIGHT)
+        from(GET_A_FLIGHT_ENDPOINT)
                 .routeId("get-a-flight-from-our-service")
                 .routeDescription("Recupérer un avion a partir du service document de notre équipe")
                 .log("interception d'une requete")
                 .setHeader(Exchange.HTTP_METHOD, constant("POST"))
                 .setHeader("Content-Type", constant("application/json"))
                 .setHeader("Accept", constant("application/json"))
-                .process((Exchange exc) -> {
-                    FlightIDRequest req = new FlightIDRequest("REGISTER",(Flight) exc.getIn().getBody());
-                    exc.getIn().setBody(req);
+                .process((Exchange exchange) -> {
+                    String request = "{\n" +
+                            "  \"event\": \"RETRIEVE\",\n" +
+                            "\"id\":\"1\"" +"\"\n" +
+                            "}";
+                    exchange.getIn().setBody(request);
                 })
-                .inOut(REGISTRATION_ENDPOINT)
+                .inOut(GET_A_FLIGHT_ENDPOINT)
                 .unmarshal().json(JsonLibrary.Jackson, Flight.class)
         ;
 
