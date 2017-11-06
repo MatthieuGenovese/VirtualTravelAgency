4900e73036101ba020e3bbd7722451ba22a59c44
Michael Liechtensteger <lm203441@etu.unice.fr>
2017-11-06 17:16:54 +0100

    enlever toto va a la plage dans le test

---
+++ integration/technical/src/test/java/esb/flows/SpendsToManagerTest.java
@@ -0,0 +1,14 @@
+package esb.flows;
+
+import esb.flows.technical.data.*;
+import org.apache.camel.Exchange;
+import org.junit.Before;
+import org.junit.Test;
+
+import static esb.flows.technical.utils.Endpoints.*;
+
+
+public class SpendsToManagerTest extends ActiveMQTest {
+    SpendRequest sr;
+    String request;
+}
