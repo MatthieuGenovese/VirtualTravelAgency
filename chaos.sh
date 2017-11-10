3e7bedfb6d0bd9b3eed6820961850d457c34c9e2
MatthieuGenovese <gm102445@etu.unice.fr>
2017-11-10 19:50:31 +0100

    ajout du chaos

---
+++ chaos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+tableau=[submittravel travelagency-bus cars-hotels-reservation flightreservation travel-cars hotels-team1 vols-document spends]
+size=${#tableau[@]}
+
+while(true)
+do
+    num=$(($RANDOM % $size))
+    echo "Stopping container ${tableau[$num]}"
+    docker stop ${tableau[$num]}
+    sleep 10
+    echo "Restarting container ${tableau[$num]}"
+    docker start ${tableau[$num]}
+done
